4b981aff7969a2d983dcd922b426d262129d5491
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2023-04-24 11:44:43 +0200

    [Refactor] Updated configuration parsing

--- src/TraefikKobling.Worker/Configuration/Server.cs
+++ src/TraefikKobling.Worker/Configuration/Server.cs
@@ -5,5 +5,8 @@ public class Server
     public required string Name { get; set; }
     public required Uri ApiAddress { get; set; }
     public required Uri DestinationAddress { get; set; }
-    public required Dictionary<string,string> EntryPoints { get; set; }
+    public Dictionary<string, string> EntryPoints { get; set; } = new()
+    {
+        {"http", "http"}
+    };
 }
---
+++ src/TraefikKobling.Worker/Extensions/IConfigurationExtensions.cs
@@ -0,0 +1,42 @@
+using TraefikKobling.Worker.Configuration;
+
+namespace TraefikKobling.Worker.Extensions;
+
+public static class IConfigurationExtensions
+{
+    public static KoblingOptions AddKoblingOptions(this IServiceCollection services, IConfiguration configuration)
+    {
+        var servers = new List<Server>();
+        foreach (var child in configuration.GetSection("servers").GetChildren())
+        {
+            var server = new Server
+            {
+                Name = child.GetRequiredValue<string>(nameof(Server.Name)),
+                ApiAddress = new Uri(child.GetRequiredValue<string>(nameof(Server.ApiAddress))),
+                DestinationAddress = new Uri(child.GetRequiredValue<string>(nameof(Server.DestinationAddress))),
+            };
+            
+            if (child.GetSection("entryPoints").Get<Dictionary<string,string>>() is { Count: > 0 } entryPoints)
+                server.EntryPoints = entryPoints;
+
+            servers.Add(server);
+        }
+        
+        var options = new KoblingOptions
+        {
+            Servers = servers.ToArray(),
+            RunEvery = configuration.GetValue<int>("RUN_EVERY")
+        };
+
+        services.AddSingleton(options);
+        return options;
+    }
+    
+    public static T GetRequiredValue<T>(this IConfiguration section, string key)
+    {
+        var value = section.GetValue<T>(key);
+        if (value is null)
+            throw new ArgumentException($"{key} is required");
+        return value;
+    }
+}
